a023b84e0bcbbceb3a7ca38ffb3d7f83150f5f64
bengera <61493854+bengera@users.noreply.github.com>
2021-05-20 13:45:36 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const headerAnimate = document.querySelector('.header__main')
+const introAnimate = document.querySelector('.header__intro--text')
+
+window.addEventListener('load', () => {
+    console.log('loaded');
+    headerAnimate.classList.add('show');
+})
+
+window.addEventListener('load', () => {
+    console.log('loaded');
+    introAnimate.classList.add('show');
+})
+
+
+
+
